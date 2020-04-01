fe6ea19a5641e2d8757087a688b4d20f4ebe01ba
lazydooog <444836458@qq.com>
2020-04-01 11:31:25 +0800

    update stack.cpp

---
+++ 数据结构/栈与队列/stack.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Description: 栈 数组实现
+ * @Author: Xiaobin Ren
+ * @Date: 2020-04-01 11:30:18
+ * @LastEditTime: 2020-04-01 11:30:51
+ */
+
+// tt表示栈顶
+int stk[N], tt = 0;
+
+// 向栈顶插入一个数
+stk[ ++ tt] = x;
+
+// 从栈顶弹出一个数
+tt -- ;
+
+// 栈顶的值
+stk[tt];
+
+// 判断栈是否为空
+if (tt > 0)
+{
+
+}
